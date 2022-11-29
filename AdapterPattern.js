13aa415986e318027cf65ec743a54ffa6f6833d3
Mike Petrusha <petrushamike@gmail.com>
2022-11-29 17:29:46 +0300

    feat: add Adapter Pattern file

---
+++ AdapterPattern.js
@@ -0,0 +1,42 @@
+class OldCalc {
+  operations(t1, t2, operation) {
+    switch (operation) {
+      case "add":
+        return t1 + t2;
+      case "sub":
+        return t1 - t2;
+      default:
+        return NaN;
+    }
+  }
+}
+
+class NewCalc {
+  add(t1, t2) {
+    return t1 + t2;
+  }
+
+  sub(t1, t2) {
+    return t1 - t2;
+  }
+}
+
+class CalcAdapter {
+  constructor() {
+    this.calc = new NewCalc();
+  }
+
+  operations(t1, t2, operation) {
+    switch (operation) {
+      case "add":
+        return this.calc.add(t1, t2);
+      case "sub":
+        return this.calc.sub(t1, t2);
+      default:
+        return NaN;
+    }
+  }
+}
+
+let calc = new CalcAdapter();
+console.log(calc.operations(10, 2, "add")); // 12
